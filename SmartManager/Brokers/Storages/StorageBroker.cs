751ca9cd2c11c17331260575b112ba8fb932b351
Zafar <icom@MACKBOOK>
2023-11-09 10:37:32 +0300

    POC: Test Project(WIP)

--- SmartManager/Brokers/Storages/StorageBroker.cs
+++ SmartManager/Brokers/Storages/StorageBroker.cs
@@ -14,47 +14,38 @@ namespace SmartManager.Brokers.Storages
 {
     public partial class StorageBroker : EFxceptionsContext, IStorageBroker
     {
-        private readonly IConfiguration configuration;
 
-        public StorageBroker(IConfiguration configuration)
+        public StorageBroker()
         {
-            this.configuration = configuration;
-            this.Database.Migrate();
+            this.Database.EnsureCreated();
         }
 
         public async ValueTask<T> InsertAsync<T>(T @object)
         {
-            try
-            {
-                var broker = new StorageBroker(this.configuration);
+                var broker = new StorageBroker();
                 broker.Entry(@object).State = EntityState.Added;
                 await broker.SaveChangesAsync();
 
                 return @object;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
         }
 
         public IQueryable<T> SelectAll<T>() where T : class
         {
-            var broker = new StorageBroker(configuration);
+            var broker = new StorageBroker();
 
             return broker.Set<T>();
         }
 
         public async ValueTask<T> SelectAsync<T>(params object[] objectsId) where T : class
         {
-            var broker = new StorageBroker(configuration);
+            var broker = new StorageBroker();
 
             return await broker.FindAsync<T>(objectsId);
         }
 
         public async ValueTask<T> UpdateAsync<T>(T @object)
         {
-            var broker = new StorageBroker(configuration);
+            var broker = new StorageBroker();
             broker.Entry(@object).State = EntityState.Modified;
             await broker.SaveChangesAsync();
 
@@ -65,7 +56,7 @@ namespace SmartManager.Brokers.Storages
         {
             try
             {
-                var broker = new StorageBroker(configuration);
+                var broker = new StorageBroker();
                 broker.Entry(@object).State = EntityState.Deleted;
                 await broker.SaveChangesAsync();
 
