71519ffea44b8ca87cb9105210266cb2469d9704
Zafar <icom@MACKBOOK>
2023-11-10 11:35:27 +0300

    POC: Test Telegram Bot

--- SmartManager/Services/Foundations/TelegramBots/TelegramBotService.cs
+++ SmartManager/Services/Foundations/TelegramBots/TelegramBotService.cs
@@ -5,11 +5,13 @@
 
 using Microsoft.Extensions.Logging;
 using SmartManager.Brokers.Telegrams;
+using SmartManager.Models.Students;
 using SmartManager.Models.TelegramInformations;
 using SmartManager.Services.Processings.Students;
 using SmartManager.Services.Processings.TelegramInformations;
 using System;
 using System.Linq;
+using System.ServiceModel.Channels;
 using System.Threading.Tasks;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
@@ -54,6 +56,79 @@ namespace SmartManager.Services.Foundations.TelegramBots
             }
         }
 
+        public async ValueTask SendAttendanceMassageToStudents(Student student, bool IsPresent)
+        {
+            var telegramInformation = this.telegramInformationProcessingService
+                .RetrieveAllTelegramInformations().FirstOrDefault(t => t.StudentId == student.Id);
+            if (IsPresent is true)
+            {
+                await this.telegramBroker.SendTextMessageAsync(
+                       telegramInformation.TelegramId,
+                       $"{student.GivenName} {student.Surname} is present!");
+            }
+            else
+            {
+                await this.telegramBroker.SendTextMessageAsync(
+                      telegramInformation.TelegramId,
+                      $"{student.GivenName} {student.Surname} is not present!");
+            }
+        }
+
+        private async ValueTask BotOnMessageRecieved(Telegram.Bot.Types.Message message)
+        {
+            var student = this.studentProcessingService
+                .RetrieveAllStudents().FirstOrDefault(s => s.PhoneNumber == message.Text);
+
+            this.logger.LogInformation($"A message has arrieved: {message.Type}");
+
+            if (message.Text is not null)
+            {
+                if (IsPhoneNumber((message.Text)))
+                {
+                    if (student is null)
+                    {
+                        await this.telegramBroker.SendTextMessageAsync(
+                            message.Chat.Id,
+                            $"Sorry, you are in our database. Contact support.");
+                    }
+                    else
+                    {
+                        await this.telegramBroker.SendTextMessageAsync(
+                            message.Chat.Id,
+                            $"Thank you {message.Chat.FirstName} " +
+                            $"{message.Chat.FirstName}, you will receive a progress report.");
+
+                        TelegramInformation telegramInformation = new TelegramInformation
+                        {
+                            Id = Guid.NewGuid(),
+                            TelegramId = message.Chat.Id,
+                            Message = message.Text,
+                            StudentId = student.Id,
+                        };
+
+                        await this.telegramInformationProcessingService
+                            .AddTelegramInformationAsync(telegramInformation);
+                    }
+                }
+                else
+                {
+                    await this.telegramBroker.SendTextMessageAsync(
+                        message.Chat.Id,
+                        $"Welcome to Smart Manager, please send us the phone number. ");
+                }
+            }
+        }
+        private bool IsPhoneNumber(string text)
+        {
+            if (text.StartsWith("+") || long.TryParse(text, out _))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
         public ValueTask HandleErrorAsync(Exception ex)
         {
             var errorMessage = ex switch
@@ -80,57 +155,6 @@ namespace SmartManager.Services.Foundations.TelegramBots
             await telegramBroker.SendTextMessageAsync(
                 callbackQuery.Message.Chat.Id,
                 $"{callbackQuery.Data}");
-        }
-        private async ValueTask BotOnMessageRecieved(Message message)
-        {
-            var student = this.studentProcessingService
-                .RetrieveAllStudents().FirstOrDefault(s => s.PhoneNumber == message.Text);
-
-            this.logger.LogInformation($"A message has arrieved: {message.Type}");
-
-            if (message.Text is not null)
-            {
-                if (IsPhoneNumber((message.Text)))
-                {
-                    if (student is null)
-                    {
-                        await this.telegramBroker.SendTextMessageAsync(
-                            message.Chat.Id,
-                            $"Sorry, you are in our database. Contact support.");
-                    }
-                    else
-                    {
-                        await this.telegramBroker.SendTextMessageAsync(
-                            message.Chat.Id,
-                            $"Thank you {message.Chat.FirstName} {message.Chat.FirstName}, you will receive a progress report.");
-
-                        TelegramInformation telegramInformation = new TelegramInformation
-                        {
-                            Id = Guid.NewGuid(),
-                            TelegramId = message.Chat.Id,
-                            Message = message.Text,
-                            StudentId = student.Id,
-                        };
-
-                        await this.telegramInformationProcessingService.AddTelegramInformationAsync(telegramInformation);
-                    }
-                }
-                else
-                {
-                    await this.telegramBroker.SendTextMessageAsync(
-                        message.Chat.Id,
-                        $"Welcome to Smart Manager, please send us the phone number. ");
-                }
-            }
-        }
-        private bool IsPhoneNumber(string text)
-        {
-            if (text.StartsWith("+") || long.TryParse(text, out _))
-            {
-                return true;
-            }
-
-            return false;
         }
     }
 }
